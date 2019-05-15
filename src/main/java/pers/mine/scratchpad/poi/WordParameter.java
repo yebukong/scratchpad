2f2470c94cab5a0ec9d51861ebd3a996f5ed8c1d
Mine <yebukong@qq.com>
2019-05-16 01:02:36 +0800

    docx文件读取测试

---
+++ src/main/java/pers/mine/scratchpad/poi/WordParameter.java
@@ -0,0 +1,61 @@
+package pers.mine.scratchpad.poi;
+
+import java.io.Serializable;
+
+/**
+ * Word参数替换，可替换参数项
+ */
+public abstract class WordParameter<T> implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	protected String name;
+	protected T value;
+	
+	/**
+	 * 构建一个参数项
+	 * @param name
+	 * @param value
+	 */
+	public WordParameter(String name, T value) {
+		this.name = name;
+		this.value = value;
+	}
+	
+	/**
+	 * 获取名称
+	 * @return
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * 设置名称
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * 获取值
+	 * @return
+	 */
+	public T getValue() {
+		return value;
+	}
+	
+	/**
+	 * 设置值
+	 * @param value
+	 */
+	public void setValue(T value) {
+		this.value = value;
+	}
+	
+	/**
+	 * 以字串形式返回值
+	 */
+	public abstract String getStringValue();
+}
