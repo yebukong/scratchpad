fdf9f4afdc0e27eba261becf2e8c1103b2a37296
Mine <yebukong@qq.com>
2019-07-11 21:11:42 +0800

    λ

---
+++ src/main/java/pers/mine/scratchpad/lambda/FunactionDemoD.java
@@ -0,0 +1,13 @@
+package pers.mine.scratchpad.lambda;
+
+/**
+ * @author Mine
+ * @date 2019/07/11 14:59:37
+ */
+@FunctionalInterface
+public interface FunactionDemoD {
+	/**
+	 * 目标抽象方法
+	 */
+	public abstract String test();
+}
