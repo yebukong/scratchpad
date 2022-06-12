df994cc6cdc89f167fb7ba493d6b8176e2553fba
yebukong <yebukong@qq.com>
2022-06-13 03:15:22 +0800

    upd

--- scratchpad-simple/src/main/java/pers/mine/scratchpad/base/classload/ClassLoadTest2.java
+++ scratchpad-simple/src/main/java/pers/mine/scratchpad/base/classload/ClassLoadTest2.java
@@ -1,6 +1,14 @@
 package pers.mine.scratchpad.base.classload;
 
+import java.sql.DriverManager;
 
 public class ClassLoadTest2 {
+    /**
+     * JDBC SPI 破坏双亲委派
+     */
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getContextClassLoader());
+        System.out.println(DriverManager.class.getClassLoader());
+    }
 
 }
